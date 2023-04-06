b5ef30e8ccf243e44b47d50fc6217ac5433afc2a
Diego Napoli <d.napoli.dev@gmail.com>
2023-04-06 15:28:32 -0300

    Add more models to the database

--- backend/app/app/models.py
+++ backend/app/app/models.py
@@ -18,3 +18,34 @@ class Customer(models.Model):
     email = models.CharField(max_length=255, null=False)
     user_type = models.ForeignKey(CustomerType, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True, null=False)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+
+class ExemplaryType(models.Model):
+    class TheType(models.TextChoices):
+        BOOK = "book"
+        NEWSPAPER = "newspaper"
+        MAGAZINE = "magazine"
+
+    type = models.CharField(max_length=10, choices=TheType.choices, default=TheType.BOOK, null=False)
+
+
+class Exemplary(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    exemplary_type = models.ForeignKey(ExemplaryType, on_delete=models.CASCADE)
+
+
+class ExemplaryAvailables(models.Model):
+    exemplary = models.ForeignKey(Exemplary, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+
+class Reservation(models.Model):
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+    reserved_customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    exemplary = models.ForeignKey(Exemplary, on_delete=models.CASCADE)
